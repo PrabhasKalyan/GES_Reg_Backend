800a630338c1ab375f57ca8895fcd657b2321799
PrabhasKalyan <prabhaskalyan0473@gmail.com>
2025-01-28 15:15:08 +0530

    update models

--- ges_portal/portal/models.py
+++ ges_portal/portal/models.py
@@ -7,7 +7,7 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 
 class Users(User):
-    phone_number=models.CharField(max_length=12)
+    phone_number=models.CharField(max_length=15)
     roles=[
         ("Student","Student"),
         ("CA","CA"),
